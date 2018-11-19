62f1a3d5e9361022bec60a782b26b09d7d6a1830
iprcc <iprcc@22BKPK2JCCAS114>
2018-11-19 15:25:46 +0800

    第一次提交代码

---
+++ mygitdemo/src/com/oracle/Test1.java
@@ -0,0 +1,17 @@
+package com.oracle;
+/**
+ * ceshi git 
+ * 第一次部署git会报错
+ *   报错跟我们的myeclipse的配置有关系
+ *     大家看演示
+ *     
+ * @author 刘庆东
+ * mygitdemo
+ * 2018年11月19日  下午3:22:32
+ */
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("工程一会报错，不是操作者的问题！！");
+	}
+
+}
