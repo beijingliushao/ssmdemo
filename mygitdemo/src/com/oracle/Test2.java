ae6d3591524a3cfabba01f12ec587ddcbe928684
iprcc <iprcc@22BKPK2JCCAS114>
2018-11-19 15:41:39 +0800

    第二次提交

---
+++ mygitdemo/src/com/oracle/Test2.java
@@ -0,0 +1,18 @@
+package com.oracle;
+/**
+ * 测试代码 
+ * 
+ * ceshi
+ * 
+ * @author 刘庆东
+ * mygitdemo
+ * 2018年11月19日  下午3:41:01
+ */
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
